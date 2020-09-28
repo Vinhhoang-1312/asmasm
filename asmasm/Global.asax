189c1444e2efd03e92f792fc32c1bdf148f73b89
vinhhoang <tranphanvinhhoang7@gmail.com>
2020-09-28 21:13:18 +0700

    first commit

---
+++ asmasm/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="asmasm.MvcApplication" Language="C#" %>
---
+++ asmasm/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace asmasm.Models
+{
+	public class Product
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		[DisplayName("Product Name")]
+		public string Name { get; set; }
+
+		[Required]
+		[DisplayName("Product Price")]
+		public double Price { get; set; }
+		[Required]
+		public int CategoryId { get; set; }
+		public ApplicationUser Author { get; set; }
+	}
+}
